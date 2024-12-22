1b3dc521cebd130a8d9619bed4f8b2d8666b6e5a
Louis Marceron <louis.marceron@protonmail.com>
2024-12-22 16:56:15 +0100

    setup dart project

---
+++ test/challenge_01_test.dart
@@ -0,0 +1,11 @@
+import 'package:cryptopals_dart/';
+import 'package:test/test.dart';
+
+void main() {
+  group('Challenge 1', () {
+    test('Hex to Base64 conversion', () {
+      const hexInput =
+          '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d';
+    });
+  });
+}
